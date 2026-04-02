19f6b5a2b9ec57ebce419026354b0bde505c58f8
Nethru Bhargavi <bhargavijn24@gmail.com>
2026-04-02 12:37:38 +0530

    fix API URL

--- frontend/src/components/VoiceTest.jsx
+++ frontend/src/components/VoiceTest.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react'
 
-const API_BASE = 'http://localhost:8000'
+const API_BASE = 'https://parkisense-zcub.onrender.com'
 
 function VoiceTest() {
   const [isRecording, setIsRecording] = useState(false)
